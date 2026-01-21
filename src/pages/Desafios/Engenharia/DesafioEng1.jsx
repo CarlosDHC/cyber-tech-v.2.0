89ff23ba74ed11aa42b1b5a117f92abe778fcd13
Arthur Pessoa da Silva <arthurpessoadasilva192@gmail.com>
2026-01-21 11:18:08 -0300

    Update DesafioEng1.jsx

--- src/pages/Desafios/Engenharia/DesafioEng1.jsx
+++ src/pages/Desafios/Engenharia/DesafioEng1.jsx
@@ -1,15 +1,10 @@
-
-function DesafioEng2() {
-  return (
-    <div>
-      <h1>Desafio Direito 1</h1>
 import React, { useState, useEffect } from "react";
 import "./DesafioEng.css";
 import { db, auth } from "../../../../FirebaseConfig";
 import { collection, addDoc } from "firebase/firestore";
 import { Link } from "react-router-dom";
 
-export default function DesafioEng1() {
+export default function DesafioEng2() {
   const total = 6;
   const corretas = ["b", "c", "a", "b", "c", "a"];
 
@@ -185,7 +180,7 @@ export default function DesafioEng1() {
         </div>
       )}
 
-<div className="navigation-links">
+      <div className="navigation-links">
         <Link to="/desafios/CapitulosDireito" className="back-link">
           <img src="/flecha1.png" alt="Voltar" className="logo-img" />
           Voltar
@@ -200,11 +195,6 @@ export default function DesafioEng1() {
           <img src="/flecha2.png" alt="Próximo" className="logo-img" />
         </Link>
       </div>
-
     </div>
   );
 }
-
-export default DesafioEng2;
-
-
